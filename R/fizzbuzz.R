78818b5ce343954db324dcbaaf4977d66d1bb5e3
nicjlambert <lambni85@outlook.com>
2022-12-11 23:21:48 +1100

    updated func

--- R/fizzbuzz.R
+++ R/fizzbuzz.R
@@ -1,6 +1,6 @@
 #' divisibleBy
 #'
-#' A program that prints the numbers from 1 to 100. But for multiples of
+#' A program that prints the numbers from 1 to n. But for multiples of
 #' three prints "Fizz" instead of the number and for the multiples of five print
 #' "Buzz". For numbers which are multiples of both three and five print "FizzBuzz".
 #'
