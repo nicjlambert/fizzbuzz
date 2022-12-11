4b53769538c580c9e014e4e87ae82600ea46b593
nicjlambert <lambni85@outlook.com>
2022-12-11 21:34:35 +1100

    updated divisibleBy

--- R/fizzbuzz.R
+++ R/fizzbuzz.R
@@ -4,29 +4,45 @@
 #' three prints "Fizz" instead of the number and for the multiples of five print
 #' "Buzz". For numbers which are multiples of both three and five print "FizzBuzz".
 #'
-#' @param range
-#' @param triggers
+#' @param range a vector of integer values
+#' @param triggers a vector of integer values, representing the divisors for which
+#'                 "Fizz" and "Buzz" will be printed
 #'
-#' @return
+#' @return a vector of strings, with the "FizzBuzz" values as described in the function
+#'         description
 #' @export divisibleBy
 #'
 #' @examples
+#' divisibleBy(1:100, c(3, 5))
+
 divisibleBy <- function(range, triggers) {
-	output <- vector()
+  # Initialize the output vector with empty strings
+  output <- rep("", length(range))
 
-	for (i in range) {
-		output[i] <- ""
+  # Loop through the range of numbers
+  for (i in range) {
+    # If the current number is divisible by both of the triggers,
+    # set the output value to "FizzBuzz"
+    if (i %% triggers[1] == 0 && i %% triggers[2] == 0) {
+      output[i] <- "FizzBuzz"
+    }
+    # If the current number is only divisible by the first trigger,
+    # set the output value to "Fizz"
+    else if (i %% triggers[1] == 0) {
+      output[i] <- "Fizz"
+    }
+    # If the current number is only divisible by the second trigger,
+    # set the output value to "Buzz"
+    else if (i %% triggers[2] == 0) {
+      output[i] <- "Buzz"
+    }
+    # If the current number is not divisible by either of the triggers,
+    # set the output value to the original number as a string
+    else {
+      output[i] <- as.character(i)
+    }
+  }
 
-		if (i %% triggers[1] == 0) {
-			output[i] <- paste0(output[i], "Fizz")
-		}
-		if (i %% triggers[2] == 0) {
-			output[i] <- paste0(output[i], "Buzz")
-		}
-		if (output[i] == "") {
-			output[i] <- i
-		}
-	}
-	print(output)
-	return(output)
+  # Return the output vector
+  return(output)
 }
